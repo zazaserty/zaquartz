21e83792d0b6e9bd78d4d7a1f765d684499e8d62
ZAZA <zazaserty@disroot.org>
2024-11-29 23:12:08 +0100

    Update quartz.layout.ts
    Fixed repetition of page title.

--- quartz.layout.ts
+++ quartz.layout.ts
@@ -25,7 +25,6 @@ export const defaultContentPageLayout: PageLayout = {
     Component.TagList(),
   ],
   left: [
-    Component.PageTitle(),
     Component.MobileOnly(Component.Spacer()),
     Component.Search(),
     Component.Darkmode(),
@@ -42,7 +41,6 @@ export const defaultContentPageLayout: PageLayout = {
 export const defaultListPageLayout: PageLayout = {
   beforeBody: [Component.Breadcrumbs(), Component.ArticleTitle(), Component.ContentMeta()],
   left: [
-    Component.PageTitle(),
     Component.MobileOnly(Component.Spacer()),
     Component.Search(),
     Component.Darkmode(),
