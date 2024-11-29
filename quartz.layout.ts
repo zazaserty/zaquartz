811cd94aca5ce84666366b659770d0d17015151a
ZAZA <zazaserty@disroot.org>
2024-11-29 21:22:05 +0100

    Update quartz.layout.ts

--- quartz.layout.ts
+++ quartz.layout.ts
@@ -42,7 +42,6 @@ export const defaultContentPageLayout: PageLayout = {
 export const defaultListPageLayout: PageLayout = {
   beforeBody: [Component.Breadcrumbs(), Component.ArticleTitle(), Component.ContentMeta()],
   left: [
-    Component.PageTitle(),
     Component.MobileOnly(Component.Spacer()),
     Component.Search(),
     Component.Darkmode(),
